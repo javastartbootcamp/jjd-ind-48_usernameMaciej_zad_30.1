146382d44e47ae00f2f4b0aa7ca1556e70f774ae
usernameMaciej <maciej.przystarz@protonmail.com>
2023-05-18 17:22:34 +0200

    rozwiazanie

--- src/main/java/pl/javastart/validationtask/RegisterFormDto.java
+++ src/main/java/pl/javastart/validationtask/RegisterFormDto.java
@@ -1,14 +1,38 @@
 package pl.javastart.validationtask;
 
+import jakarta.validation.constraints.*;
+
 public class RegisterFormDto {
 
+    @NotBlank(message = "Imię nie może być puste")
+    @Min(value = 3, message = "Imię musi mieć minimum 3 znaki")
     private String firstName;
+
+    @NotBlank(message = "Nazwisko nie może być puste")
+    @Min(value = 3, message = "Nazwisko musi mieć minimum 3 znaki")
     private String surname;
+
+    @NotBlank(message = "Adres nie może być pusty")
     private String address;
+
+    @NotBlank(message = "Kod pocztowy nie może być pusty")
+    @Pattern(regexp = "[0-9]{5}", message = "Kod pocztowy musi wyglądać w taki sposób \"12-345\"")
     private String postalCode;
+
+    @NotBlank(message = "Miasto nie może być puste")
     private String city;
+
+    @NotNull(message = "Email nie może być pusty")
+    @Email(message = "Wpisz prawidłowy e-mail")
+    @Pattern(regexp = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", message = "Wpisz prawidłowy e-mail")
     private String email;
+
+    @NotNull(message = "Hasło nie może być puste")
+    @Min(value = 8, message = "Hasło musi zawierać minimum 8 znaków")
+    @Pattern(regexp = "[a-zA-Z0-9]*", message = "Hasło musi zawierać min 1 duży znak, 1 mały, 1 znak specjalny")
     private String password;
+
+    @AssertTrue(message = "Akceptacja regulaminu jest wymagana")
     private boolean termsAgreement;
 
     public String getFirstName() {
